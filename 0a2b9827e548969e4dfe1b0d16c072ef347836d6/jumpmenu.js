56fd49b35d6d473fdc9e3005be7ba13cce105c1e
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2022-09-29 13:48:19 +0200

    fix css

--- 0a2b9827e548969e4dfe1b0d16c072ef347836d6/jumpmenu.js
+++ 0a2b9827e548969e4dfe1b0d16c072ef347836d6/jumpmenu.js
@@ -1,6 +1,6 @@
 function createSubMenu(){
     var jumpMenu = document.getElementById("jumpMenu")
-    headlines = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+    headlines = document.querySelectorAll("h1, h2");
     let subMenu = ""
     headlines.forEach((headlineItem) => {
       subMenu = subMenu + `<li><a href="#${headlineItem.id}">${headlineItem.innerHTML}</a></li>`
