c70b3132b6ee6c79f9761a1412d51a09f21a789e
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2022-10-05 20:31:44 +0200

    docs

--- create_pdf.sh
+++ create_pdf.sh
@@ -12,7 +12,7 @@
 ROOT_DIR=94921f622d08ab26f04c534104a6260ef8bafd78
 FILENAME=documentation_geonode4_csgis.pdf
 rm $ROOT_DIR/$FILENAME
-wkhtmltopdf --disable-javascript --footer-right [page]/[topage] --user-style-sheet ./printstyle.css \
+wkhtmltopdf --image-dpi 100 --disable-javascript --footer-right [page]/[topage] --user-style-sheet ./printstyle.css \
     "https://docs.csgis.de/94921f622d08ab26f04c534104a6260ef8bafd78" \
     "https://docs.csgis.de/94921f622d08ab26f04c534104a6260ef8bafd78/user/index.html" \
     "https://docs.csgis.de/94921f622d08ab26f04c534104a6260ef8bafd78/user/user_profil.html" \
