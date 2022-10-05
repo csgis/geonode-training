c1c14fa87044691c073f2f4dd18f33efd93f5b7a
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2022-10-05 13:11:21 +0200

    docs

--- 94921f622d08ab26f04c534104a6260ef8bafd78/menu.js
+++ 94921f622d08ab26f04c534104a6260ef8bafd78/menu.js
@@ -1,3 +1,21 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : sParameterName[1];
+      }
+  }
+};
+
+
+
+
 const URL = window.location.href;
 const fileName = URL.split("/").pop();
 
@@ -29,4 +47,6 @@ async function loadNames() {
     menu.innerHTML = menuDom
 }
 
-loadNames()
+var print_pdf = getUrlParameter('pdf');
+if (!print_pdf)
+  loadNames();
